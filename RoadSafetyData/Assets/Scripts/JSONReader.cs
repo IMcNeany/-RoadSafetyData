e30556b6f9681d0ac28b22c1e72f51077977b5f4
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2019-10-17 15:01:49 +0100

    Fixed Json importing, had to change some chars on import to match class names, will have to do this with future imports

--- RoadSafetyData/Assets/Scripts/JSONReader.cs
+++ RoadSafetyData/Assets/Scripts/JSONReader.cs
@@ -5,63 +5,63 @@ using System.IO;
 using UnityEngine;
 
 [System.Serializable]
-public class Rootobject
+public class Items
 {
-
-    public Items[] jsonObject { get; set; }
+    public string Accident_Index;
+    public string Vehicle_Reference;
+    public string Casualty_Reference;
+    public string Casualty_Class;
+    public string Sex_of_Casualty;
+    public string Age_of_Casualty;
+    public string Age_Band_of_Casualty;
+    public string Casualty_Severity;
+    public string Pedestrian_Location;
+    public string Pedestrian_Movement;
+    public string Car_Passenger;
+    public string Bus_or_Coach_Passenger;
+    public string Pedestrian_Road_Maintenance_Worker;
+    public string Casualty_Type;
+    public string Casualty_Home_Area_Type;
+    public string Casualty_IMD_Decile;
+    public string Location_Easting_OSGR;
+    public string Location_Northing_OSGR;
+    public string Longitude;
+    public string Latitude;
+    public string Police_Force;
+    public string Accident_Severity;
+    public string Number_of_Vehicles;
+    public string Number_of_Casualties;
+    public string Date;
+    public string Day_of_Week;
+    public string Time;
+    public string Local_Authority_District;
+    public string Local_Authority_Highway;
+    public string Road_class_one;
+    public string Road_number_one;
+    public string Road_Type;
+    public string Speed_limit;
+    public string Junction_Detail;
+    public string Junction_Control;
+    public string Road_class_two;
+    public string Road_number_two;
+    public string Pedestrian_Crossing_Human_Control;
+    public string Pedestrian_Crossing_Physical_Facilities;
+    public string Light_Conditions;
+    public string Weather_Conditions;
+    public string Road_Surface_Conditions;
+    public string Special_Conditions_at_Site;
+    public string Carriageway_Hazards;
+    public string Urban_or_Rural_Area;
+    public string Did_Police_Officer_Attend_Scene_of_Accident;
+    public string LSOA_of_Accident_Location;
 }
 
 [System.Serializable]
-public class Items
+public class Rootobject
 {
-    public string Accident_Index { get; set; }
-    public string Vehicle_Reference { get; set; }
-    public string Casualty_Reference { get; set; }
-    public string Casualty_Class { get; set; }
-    public string Sex_of_Casualty { get; set; }
-    public string Age_of_Casualty { get; set; }
-    public string Age_Band_of_Casualty { get; set; }
-    public string Casualty_Severity { get; set; }
-    public string Pedestrian_Location { get; set; }
-    public string Pedestrian_Movement { get; set; }
-    public string Car_Passenger { get; set; }
-    public string Bus_or_Coach_Passenger { get; set; }
-    public string Pedestrian_Road_Mastringenance_Worker { get; set; }
-    public string Casualty_Type { get; set; }
-    public string Casualty_Home_Area_Type { get; set; }
-    public string Casualty_IMD_Decile { get; set; }
-    public string Location_Easting_OSGR { get; set; }
-    public string Location_Northing_OSGR { get; set; }
-    public string Longitude { get; set; }
-    public string Latitude { get; set; }
-    public string Police_Force { get; set; }
-    public string Accident_Severity { get; set; }
-    public string Number_of_Vehicles { get; set; }
-    public string Number_of_Casualties { get; set; }
-    public string Date { get; set; }
-    public string Day_of_Week { get; set; }
-    public string Time { get; set; }
-    public string Local_Authority_District { get; set; }
-    public string Local_Authority_Highway { get; set; }
-    public string _1st_Road_Class { get; set; }
-    public string _1st_Road_Number { get; set; }
-    public string Road_Type { get; set; }
-    public string Speed_limit { get; set; }
-    public string Junction_Detail { get; set; }
-    public string Junction_Control { get; set; }
-    public string _2nd_Road_Class { get; set; }
-    public string _2nd_Road_Number { get; set; }
-    public string Pedestrian_CrossingHuman_Control { get; set; }
-    public string Pedestrian_CrossingPhysical_Facilities { get; set; }
-    public string Light_Conditions { get; set; }
-    public string Weather_Conditions { get; set; }
-    public string Road_Surface_Conditions { get; set; }
-    public string Special_Conditions_at_Site { get; set; }
-    public string Carriageway_Hazards { get; set; }
-    public string Urban_or_Rural_Area { get; set; }
-    public string Did_Police_Officer_Attend_Scene_of_Accident { get; set; }
-    public string LSOA_of_Accident_Location { get; set; }
+    public List<Items> jsonObject;
 }
+
 
 public class JSONReader : MonoBehaviour
 {
@@ -84,7 +84,7 @@ public class JSONReader : MonoBehaviour
             string dataAsJson = File.ReadAllText(filePath);
             // Pass the json to JsonUtility, and tell it to create a GameData object from it
              loadedData = JsonUtility.FromJson<Rootobject>("{\"jsonObject\":" + dataAsJson + "}");
-            Debug.Log(loadedData.jsonObject[0].Speed_limit);
+            
         }
         else
         {
@@ -104,9 +104,9 @@ public class JSONReader : MonoBehaviour
     {
 
         string JsonObject = loadedData.jsonObject[objectNo].ToString();
-            //loadedData.jsonObject[objectNo].ToString();
+        //loadedData.jsonObject[objectNo].ToString();
+        // Debug.Log(loadedData.jsonObject[0].Pedestrian_CrossingPhysical_Facilities.ToString());
 
-        Debug.Log("JSON: " + JsonObject);
         //in a loop
         //usableData new data
         //new data.time = JsonObject.Items[3]["time"].Tostring;
