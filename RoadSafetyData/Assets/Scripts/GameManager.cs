fd7c84dae1a7c587296b9eafb13e14d768b67237
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2019-10-17 01:12:51 +0100

    making test scene with test variables

---
+++ RoadSafetyData/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int                  speed_of_cars = 0;
+    public weather_conditions   current_weather;
+    public int                  current_day;
+    public int                  current_time;
+//----------------------------------------------------------------
+    public float                default_hit_chance;
+    [SerializeField]
+    private List<float>         weather_multipliers;
+    [SerializeField]
+    private List<float>         day_multipliers;
+    [SerializeField]
+    private List<float>         time_multipliers;
+
+    private int                 num_accidents_in_year = 122636;
+    private int                 num_people_uk = 66040000;
+    public float                percentage_accident;
+
+    void Start()
+    {
+        UpdatePercentageAccident();
+    }
+
+    public void UpdatePercentageAccident()
+    {
+        percentage_accident = (float)num_accidents_in_year / (float)num_people_uk * 100.0f;
+    }
+
+    //default chance of accident without pedestrian data
+    public void UpdateHitChance()
+    {
+        UpdatePercentageAccident();
+        default_hit_chance = 0 + (speed_of_cars / 100) * weather_multipliers[(int)current_weather] * day_multipliers[current_day] * time_multipliers[current_time];
+
+    }
+}
---
+++ RoadSafetyData/Assets/Scripts/UsableData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum accident_locations
+{
+    crossing,
+    approach,
+    nearby,
+    carriageway,
+    pavement
+}
+
+public enum accident_severity
+{
+    slight,
+    serious,
+    fatal
+}
+
+public enum weather_conditions
+{
+    normal,
+    rain,
+    snow
+}
+
+public enum weekday
+{
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday
+}
+
+public enum lighting
+{
+    daylight,
+    streetlamp,
+    darkness
+}
+
+
+//Usable data is created with specific variables from the main data file
+public class UsableData : MonoBehaviour
+{
+    public accident_locations location;
+    public accident_severity severity;
+    public int age;
+    public weather_conditions weather;
+    public weekday day;
+    public int time;
+    public int speed;
+}
