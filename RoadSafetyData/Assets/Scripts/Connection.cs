126bf44a3bf080000acf92a56e4ef379a25b9b99
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2019-10-19 22:14:11 +0100

    Added crossing connections, testing movement

--- RoadSafetyData/Assets/Scripts/Connection.cs
+++ RoadSafetyData/Assets/Scripts/Connection.cs
@@ -7,12 +7,28 @@ public class Connection : MonoBehaviour
 {
     public GameObject connection_1;
     public GameObject connection_2;
+    public MoveWaypoints way_points1;
+    public MoveWaypoints way_points2;
+    public bool crossable = false;
 
     void Update()
     {
         if(connection_1 && connection_2)
         {
             Debug.DrawLine(connection_1.transform.position, connection_2.transform.position, Color.cyan);
+        }
+    }
+
+    public GameObject GetNextConnection(GameObject first_connection)
+    {
+        Debug.Log(first_connection.name);
+        if(first_connection == connection_1)
+        {
+            return connection_2;
+        }
+        else
+        {
+            return connection_1;
         }
     }
 }
--- RoadSafetyData/Assets/Scripts/PersonMovement.cs
+++ RoadSafetyData/Assets/Scripts/PersonMovement.cs
@@ -6,17 +6,30 @@ public class PersonMovement : MonoBehaviour
 {
     public float speed = 1.0f;
     public MoveWaypoints current_waypoints;
-    private GameObject current_target;
+    public Connection current_connection;
+    public GameObject connection_start;
+    public GameObject connection_end;
     public bool waiting = false;
-    private int connection_index = 0;
+    public GameObject current_target;
+    private int waypoint_index = 0;
     private bool fading = false;
-    // Start is called before the first frame update
+    private Animator animator;
+    public bool crossed = false;
+
     void Start()
     {
         current_target = current_waypoints.waypoints[0];
+        animator = GetComponentInChildren<Animator>();
+        if(current_waypoints)
+        {
+            if(current_waypoints.connection)
+            {
+                current_connection = current_waypoints.connection;
+                connection_start = current_waypoints.connection_start;
+            }
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePerson();
@@ -24,28 +37,58 @@ public class PersonMovement : MonoBehaviour
 
     private void MovePerson()
     {
-        if(!waiting)
+        int final_point = current_waypoints.waypoints.Count - 1;
+        if (current_target == current_waypoints.waypoints[final_point])
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            Vector3 target_direction = (current_target.transform.position - transform.position).normalized;
-            transform.rotation = Quaternion.LookRotation(target_direction);
-            int final_point = current_waypoints.waypoints.Count - 1;
-            if (current_target == current_waypoints.waypoints[final_point])
+            if ((Vector3.Distance(transform.position, current_target.transform.position) < 0.5f))
             {
-                if ((Vector3.Distance(transform.position, current_target.transform.position) < 0.5f))
-                {
-                    FadeAway();
-                }
-            }
-            else
-            {
-                if ((Vector3.Distance(transform.position, current_target.transform.position) < 0.5f))
-                {
-                    connection_index++;
-                }
-                current_target = current_waypoints.waypoints[connection_index];
+                FadeAway();
             }
         }
+        else
+        {
+            if ((Vector3.Distance(transform.position, current_target.transform.position) < 0.5f))
+            {
+                if(current_target == connection_start && crossed == false)
+                {
+                    if (current_connection.crossable)
+                    {
+                        connection_end = current_connection.GetNextConnection(connection_start);
+                        current_target = connection_end;
+                    }
+                    else
+                    {
+                        animator.SetBool("Moving", false);
+                        return;
+                    }
+                }
+                else
+                {
+                    if(current_target == connection_end && crossed == false)
+                    {
+                        crossed = true;
+                        if (current_waypoints == current_connection.way_points1)
+                        {
+                            current_waypoints = current_connection.way_points2;
+                        }
+                        else
+                        {
+                            current_waypoints = current_connection.way_points1;
+                        }
+                        current_target = current_waypoints.waypoints[waypoint_index];
+                    }
+                    else
+                    {
+                        waypoint_index++;
+                        current_target = current_waypoints.waypoints[waypoint_index];
+                    }
+                }
+            }
+        }
+        animator.SetBool("Moving", true);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Vector3 target_direction = (current_target.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(target_direction);
     }
 
     private void FadeAway()
