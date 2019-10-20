3c2aee254bc493d71e91b067ad6004c53eb30c12
jaquesoler <jaquesoler@gmail.com>
2019-10-20 06:11:10 -0300

    Path drawing for the spawned chickens, currently they all follow the same line

---
+++ RoadSafetyData/Assets/Scripts/ChickenMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChickenMovement : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    private Queue<Vector3> pathPoints = new Queue<Vector3>();
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        FindObjectOfType<ChickenPathDrawer>().OnNewPathCreated += SetPoints;
+    }
+
+    private void SetPoints(IEnumerable<Vector3> points)
+    {
+        pathPoints = new Queue<Vector3>(points);
+    }
+
+    void Update()
+    {
+        UpdatePathing();
+    }
+
+    private void UpdatePathing()
+    {
+        if (ShouldSetDestination())
+        {
+            navMeshAgent.SetDestination(pathPoints.Dequeue());
+        }
+    }
+
+    private bool ShouldSetDestination()
+    {
+        if (pathPoints.Count == 0)
+            return false;
+
+        if (navMeshAgent.hasPath == false || navMeshAgent.remainingDistance < 0.5f)
+            return true;
+
+        return false;
+    }
+}
