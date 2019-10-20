5d33b759e91c1c381ca5b2f368aad0dd1bb77a19
jaquesoler <jaquesoler@gmail.com>
2019-10-20 11:54:42 -0300

    HUD display fake text, fixed camera angle

---
+++ RoadSafetyData/Assets/Scripts/GameplayScripts/HUD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text data_text;
+    private string crossing_type = "pedestrian";
+    private string test = "60%";
+    private string n_accidents = "50";
+    private string severity = "deadly";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data_text.text = "Crossing Type: " + crossing_type + "\nNumber of accidents: " + n_accidents + "\nOverall severity: " + severity + "\nDeath chance: " + test;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
