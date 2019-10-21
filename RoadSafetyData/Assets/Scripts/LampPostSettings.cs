5a943fe16982a5763363498124d84bed152f4769
Isobel McNeany <icmc112@yahoo.co.uk>
2019-10-21 18:46:11 +0100

    Added houses to the scene and streetlights, and created a script to set the streetlights/ lights on/off

---
+++ RoadSafetyData/Assets/Scripts/LampPostSettings.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampPostSettings : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> lampPosts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lampPosts = new List<GameObject>();
+        lampPosts.AddRange( GameObject.FindGameObjectsWithTag("Lamp"));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisableLight()
+    {
+        ToggleLight(false);
+    }
+
+
+    public void EnableLight()
+    {
+        ToggleLight(true);
+    }
+
+   
+    private void ToggleLight(bool active)
+    {
+        for (int i = 0; i < lampPosts.Count; i++)
+        {
+            lampPosts[i].transform.GetChild(0).gameObject.SetActive(active);
+        }
+    }
+
+    public void DisableLampPosts()
+    {
+        ToggleLampPosts(false);
+    }
+
+
+    public void EnableLampPosts()
+    {
+        ToggleLampPosts(true);
+    }
+
+    private void ToggleLampPosts(bool active)
+    {
+        for (int i = 0; i < lampPosts.Count; i++)
+        {
+            lampPosts[i].SetActive(active);
+        }
+    }
+}
