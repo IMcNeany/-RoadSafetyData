2b70201e8f382d3f2d52fe42603adb5c6ff966f4
nataliaptzk <nataliaptzk@gmail.com>
2019-10-27 19:32:06 +0000

    Updated UI (sorry, I reformatted the gamemanager.cs script)

--- RoadSafetyData/Assets/Scripts/GameManager.cs
+++ RoadSafetyData/Assets/Scripts/GameManager.cs
@@ -1,31 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public int                  speed_of_cars = 0;
-    public accident_weather   current_weather;
-    public int                  current_day;
-    public int                  current_time;
-//----------------------------------------------------------------
-    public float                default_hit_chance;
-    [SerializeField]
-    private List<float>         weather_multipliers;
-    [SerializeField]
-    private List<float>         day_multipliers;
-    [SerializeField]
-    private List<float>         time_multipliers;
+    public int speed_of_cars = 0;
+    public accident_weather current_weather;
+    public int current_day;
 
-    private DataCruncher        data_cruncher;
-    private JSONReader          json_reader;
-    public float                percentage_accident;
+    public int current_time;
+
+//----------------------------------------------------------------
+    public float default_hit_chance;
+    [SerializeField] private List<float> weather_multipliers;
+    [SerializeField] private List<float> day_multipliers;
+    [SerializeField] private List<float> time_multipliers;
+
+    private DataCruncher data_cruncher;
+    private JSONReader json_reader;
+    public float percentage_accident;
 
     private float weatherChance = 0.0f;
-    private float dayChance =0;
+    private float dayChance = 0;
     private float lightingChance = 0;
-    private float severityChance =0;
+    private float severityChance = 0;
 
     [SerializeField] private GameObject _chart;
 
@@ -48,42 +49,59 @@ public class GameManager : MonoBehaviour
 
         percentage_accident = (weatherChance + dayChance + lightingChance + severityChance) / 4;
 
+        UpdateTheChart();
+    }
+
+
+    private void UpdateTheChart()
+    {
+/*        var tempFillAmount = _chart.transform.GetChild(0).GetComponent<Image>().fillAmount;
+        if (percentage_accident != tempFillAmount)
+        {
+            tempFillAmount = _chart.transform.GetChild(0).GetComponent<Image>().fillAmount;
+
+            _chart.transform.GetChild(0).GetComponent<Image>().fillAmount =
+                Mathf.Lerp(tempFillAmount, percentage_accident, Time.deltaTime * 2f);
+        }*/
+
         _chart.transform.GetChild(0).GetComponent<Image>().fillAmount = percentage_accident / 100;
+        _chart.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text =
+            Math.Round(percentage_accident, 2).ToString() + "%";
     }
 
     //default chance of accident without pedestrian data
     public void UpdateHitChance()
     {
         UpdatePercentageAccident();
-        default_hit_chance = 0 + (speed_of_cars / 100) * weather_multipliers[(int)current_weather] * day_multipliers[current_day] * time_multipliers[current_time];
-
+        default_hit_chance = 0 + (speed_of_cars / 100) * weather_multipliers[(int) current_weather] *
+                             day_multipliers[current_day] * time_multipliers[current_time];
     }
 
     public void CalculateFinalChance(int dataNo, string property)
     {
         //really bad way of doing this
 
-        switch(property)
+        switch (property)
         {
             case "weather":
-                {
-                    weatherChance = data_cruncher.weather_chances[dataNo].percentage_chance;
-                }
+            {
+                weatherChance = data_cruncher.weather_chances[dataNo].percentage_chance;
+            }
                 break;
             case "day":
-                {
-                    dayChance = data_cruncher.day_chances[dataNo].percentage_chance;
-                }
+            {
+                dayChance = data_cruncher.day_chances[dataNo].percentage_chance;
+            }
                 break;
             case "lighting":
-                {
-                    lightingChance = data_cruncher.light_chances[dataNo].percentage_chance;
-                }
+            {
+                lightingChance = data_cruncher.light_chances[dataNo].percentage_chance;
+            }
                 break;
             case "severity":
-                {
-                    severityChance = data_cruncher.fatality_chances[dataNo].percentage_chance;
-                }
+            {
+                severityChance = data_cruncher.fatality_chances[dataNo].percentage_chance;
+            }
                 break;
         }
 
