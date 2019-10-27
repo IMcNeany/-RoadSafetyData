c7e604429ff3dea60209f3efcfa27f52a6a5953a
nataliaptzk <nataliaptzk@gmail.com>
2019-10-27 23:50:30 +0000

    added start scene and quit on Q

---
+++ RoadSafetyData/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            QuitGame();
+        }
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
